40db992d73d7d834d6cada7447d5a79f8c5e3ff3
Liam Hurley <liamhurley1888@gmail.com>
2025-02-20 20:56:34 +0000

    add xg, xa & gp to relevant tables & profiles. use correcct avg rating property

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,15 +10,17 @@ export const STAT_HEADERS = {
     Overall: [
       ...BASE_HEADERS,
       { label: "Goals", key: "overallStatsDto.goals" },
+      { label: "xG", key: "overallStatsDto.expectedGoals" },
       { label: "Assists", key: "overallStatsDto.goalAssist" },
-      { label: "Avg rating", key: "overallStatsDto.rating" }
+      { label: "xA", key: "overallStatsDto.expectedAssists" },
+      { label: "Avg rating", key: "overallStatsDto.averageRating" }
     ],
     Goalkeeping: [
       ...BASE_HEADERS,
       { label: "Saves", key: "overallStatsDto.saves" },
       { label: "CS", key: "overallStatsDto.cleanSheets" },
       { label: "Passing acc %", key: "overallStatsDto.passAccuracy" },
-      { label: "Avg rating", key: "overallStatsDto.rating" }
+      { label: "Avg rating", key: "overallStatsDto.averageRating" }
     ],
     Defending: [
       ...BASE_HEADERS,
@@ -29,28 +31,31 @@ export const STAT_HEADERS = {
       { label: "Int", key: "overallStatsDto.interceptionWon" },
       { label: "Tackles", key: "overallStatsDto.totalTackle" },
       { label: "Blocks", key: "overallStatsDto.outfielderBlock" },
-      { label: "Avg rating", key: "overallStatsDto.rating" }
+      { label: "Avg rating", key: "overallStatsDto.averageRating" }
     ],
     Passing: [
       ...BASE_HEADERS,
       { label: "Passes", key: "overallStatsDto.totalPass" },
       { label: "Assists", key: "overallStatsDto.goalAssist" },
+      { label: "xA", key: "overallStatsDto.expectedAssists" },
       { label: "Passing acc %", key: "overallStatsDto.passAccuracy" },
       { label: "Long balls", key: "overallStatsDto.totalLongBalls" },
       { label: "Long ball acc %", key: "overallStatsDto.longBallAccuracy" },
       { label: "Big chances created", key: "overallStatsDto.bigChanceCreated" },
       { label: "Key passes", key: "overallStatsDto.keyPass" },
       { label: "Poss lost", key: "overallStatsDto.possessionLostCtrl" },
-      { label: "Avg rating", key: "overallStatsDto.rating" }
+      { label: "Avg rating", key: "overallStatsDto.averageRating" }
     ],
     Attacking: [
       ...BASE_HEADERS,
       { label: "Goals", key: "overallStatsDto.goals" },
+      { label: "xG", key: "overallStatsDto.expectedGoals" },
       { label: "Assists", key: "overallStatsDto.goalAssist" },
+      { label: "xA", key: "overallStatsDto.expectedAssists" },
       { label: "Shots", key: "overallStatsDto.totalShotsTaken" },
       { label: "Big chances missed", key: "overallStatsDto.bigChanceMissed" },
       { label: "Aerial win %", key: "overallStatsDto.aerialDuelsWonPercentage" },
-      { label: "Avg rating", key: "overallStatsDto.rating" }
+      { label: "Avg rating", key: "overallStatsDto.averageRating" }
     ],
 };
 
@@ -70,11 +75,12 @@ export const OUTFIELD_PROFILE_STATS = {
     { label: "Minutes", key: "minutesPlayed" },
     { label: "Goals", key: "goals" },
     { label: "Assists", key: "goalAssist" },
-    { label: "Avg rating", key: "rating" },
+    { label: "Avg rating", key: "averageRating" },
   ],
   Creativity: [
-    { label: "Passes completed", key: "accuratePass" },
+    { label: "Expected assists", key: "expectedAssists" },
     { label: "Assists", key: "goalAssist" },
+    { label: "Passes completed", key: "accuratePass" },
     { label: "Passing acc %", key: "passAccuracy" },
     { label: "Long balls", key: "totalLongBalls" },
     { label: "Long ball acc %", key: "longBallAccuracy" },
@@ -84,6 +90,7 @@ export const OUTFIELD_PROFILE_STATS = {
     { label: "Poss lost", key: "possessionLostCtrl" },
   ],
   Attacking: [
+    { label: "Expected goals", key: "expectedGoals" },
     { label: "Goals", key: "goals" },
     { label: "Shots", key: "totalShotsTaken" },
     { label: "Shots on target", key: "onTargetScoringAttempt" },
@@ -98,11 +105,12 @@ export const GK_PROFILE_STATS = [
   { label: "Appearances", key: "matchesPlayed" },
   { label: "Minutes", key: "minutesPlayed" },
   { label: "Saves", key: "saves" },
+  { label: "Goals prevented", key: "goalsPrevented" },
   { label: "Saves from shots inside box", key: "savedShotsFromInsideTheBox" },
   { label: "Clean sheets", key: "cleanSheets" },
   { label: "Passing acc %", key: "passAccuracy" },
   { label: "Errors leading to shot", key: "errorLeadToAShot" },
-  { label: "Avg rating", key: "rating" },
+  { label: "Avg rating", key: "averageRating" },
 ]
 
 export const p90ableStats = [
@@ -128,5 +136,8 @@ export const p90ableStats = [
     "overallStatsDto.outfielderBlock",
     "overallStatsDto.bigChanceCreated",
     "overallStatsDto.errorLeadToAShot",
-    "overallStatsDto.totalPass"
+    "overallStatsDto.totalPass",
+    "overallStatsDto.expectedGoals",
+    "overallStatsDto.expectedAssists",
+    "overallStatsDto.goalsPrevented",
 ];
