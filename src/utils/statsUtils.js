3752ef197a4fbaaf88af705ee69c26e8bed1b6b2
Liam Hurley <liamhurley1888@gmail.com>
2025-04-04 21:07:12 +0100

    stat wiring, this really sucks

--- src/utils/statsUtils.js
+++ src/utils/statsUtils.js
@@ -20,6 +20,68 @@ const sanitise = (statName) => {
             return "minutesPlayed";
         case "assists":
             return "goalAssist";
+        case "clean sheets":
+            return "cleanSheets";
+        case "aerial duels won":
+            return "cleanSheets";
+        case "aerial duels won %":
+            return "aerialDuelsWonPercentage";
+        case "duels won":
+            return "duelWon";
+        case "duels won %":
+            return "duelWonPercentage";
+        case "interceptions":
+            return "interceptionWon"
+        case "tackles":
+            return "totalTackle";
+        case "blocks":
+            return "outfielderBlock";
+        case "fouls":
+            return "fouls";
+        case "error lead to shot":
+            return "errorLeadToAShot";
+        case "expected assists":
+            return "expectedAssists";
+        case "passes attempted":
+            return "totalPass";
+        case "passes completed":
+            return "accuratePass";
+        case "pass completion %":
+            return "passAccuracy";
+        case "big chances created":
+            return "bigChanceCreated";
+        case "key passes":
+            return "keyPass";
+        case "long balls":
+            return "totalLongBalls";
+        case "crosses completed":
+            return "accurateCross";
+        case "possession lost":
+            return "possessionLostCtrl";
+        case "goals":
+            return "goals";
+        case "expected goals":
+            return "expectedGoals";
+        case "shots taken":
+            return "totalShotsTaken";
+        case "shots on target":
+            return "onTargetScoringAttempt";
+        case "big chances missed":
+            return "bigChanceMissed";
+        case "saves":
+            return "saves";
+        case "goals prevented":
+            return "goalsPrevented";
+        case "punches":
+            return "punches";
+        case "saved shots from inside the box":
+            return "savedShotsFromInsideTheBox";
+        case "fouled":
+            return "wasFouled";
+        case "touches":
+            return "touches";
+        case "captain":
+            return "captain";    
         default:
             return statName;
     }
